e3db87d22f82606813adf3945944255e034896fe
Apple <apple@11.local>
2016-10-22 02:40:53 -0700

    Add the function of search bar

--- CalendarGo/app/src/main/java/com/example/apple/calendargo/MainActivity.java
+++ CalendarGo/app/src/main/java/com/example/apple/calendargo/MainActivity.java
@@ -3,19 +3,31 @@ package com.example.apple.calendargo;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.*;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import android.widget.TextView;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnMenuTabSelectedListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
     private CoordinatorLayout coordinatorLayout;
     private Fragment f;
+    private SearchView mSearchView;
+    private TextView mStatusView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         f = new ListFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.frame,f).commit();
@@ -55,5 +67,32 @@ public class MainActivity extends AppCompatActivity {
         bottomBar.setActiveTabColor("#C2185B");
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
 
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
+        mSearchView = (SearchView) searchItem.getActionView();
+        mSearchView.setOnQueryTextListener(this);
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        mStatusView = (TextView) findViewById(R.id.searchShow);
+        f = new SearchFragment();
+        Bundle args = new Bundle();
+        args.putString("query_string", query);
+        f.setArguments(args);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame,f).commit();
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
 }
