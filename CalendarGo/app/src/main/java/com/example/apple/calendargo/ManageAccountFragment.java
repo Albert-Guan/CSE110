51ec4960984af92ce070b1b835c4fa6721816f2f
jd <jdr000@live.com>
2016-11-17 00:22:52 -0800

    Settings changes

--- CalendarGo/app/src/main/java/com/example/apple/calendargo/ManageAccountFragment.java
+++ CalendarGo/app/src/main/java/com/example/apple/calendargo/ManageAccountFragment.java
@@ -3,15 +3,18 @@ package com.example.apple.calendargo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by jd on 11/11/16.
  */
 
-public class ManageAccountFragment extends Fragment {
+public class ManageAccountFragment extends Fragment implements View.OnClickListener {
     @Nullable
 
     @Override
@@ -20,6 +23,36 @@ public class ManageAccountFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.manage_account, container, false);
 
+        Button b5 = (Button) v.findViewById(R.id.button5);
+        Button b4 = (Button) v.findViewById(R.id.button4);
+
+        b5.setOnClickListener(this);
+        b4.setOnClickListener(this);
+
         return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            // cancel button
+            case R.id.button5:
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Bundle args = new Bundle();
+                args.putBoolean("hasLoggedIn", MainActivity.hasLoggedIn);
+                Fragment newFragment = new MoreFragment();
+                newFragment.setArguments(args);
+
+                fragmentTransaction.replace(R.id.frame, newFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                break;
+            // save changes button
+            case R.id.button4:
+
+                break;
+            default:
+        }
     }
 }
