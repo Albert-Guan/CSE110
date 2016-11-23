872fdea094f645bd41039ee32f378e70d0c4afce
jd <jdr000@live.com>
2016-11-23 00:41:04 -0800

    Merging project

--- CalendarGo/app/src/main/java/com/example/apple/calendargo/DataFragment.java
+++ CalendarGo/app/src/main/java/com/example/apple/calendargo/DataFragment.java
@@ -3,17 +3,20 @@ package com.example.apple.calendargo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 /**
  * Created by jd on 11/11/16.
  */
 
-public class DataFragment extends Fragment {
+public class DataFragment extends Fragment implements View.OnClickListener {
     @Nullable
 
     @Override
@@ -28,8 +31,36 @@ public class DataFragment extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        Button b8 = (Button) v.findViewById(R.id.button8);
+        Button b9 = (Button) v.findViewById(R.id.button9);
+
+        b8.setOnClickListener(this);
+        b9.setOnClickListener(this);
+
         return v;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            // cancel button
+            case R.id.button8:
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Bundle args = new Bundle();
+                args.putBoolean("hasLoggedIn", MainActivity.hasLoggedIn);
+                Fragment newFragment = new MoreFragment();
+                newFragment.setArguments(args);
 
+                fragmentTransaction.replace(R.id.frame, newFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                break;
+            // force sync
+            case R.id.button9:
+
+                break;
+            default:
+        }
+    }
 }
